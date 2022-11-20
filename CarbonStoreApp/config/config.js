c5136e2da5f7bd925819bbee1c3f5967891c808f
hechen77 <2038255225@qq.com>
2022-11-20 23:44:33 +0800

    update:APP增加登陆过期验证

--- CarbonStoreApp/config/config.js
+++ CarbonStoreApp/config/config.js
@@ -1,27 +1,50 @@
 const serverUrl = "http://carbon.hc8.ren/app";
-const localUrl = "http://localhost:3000/app"
+const localUrl = "http://localhost:3000/app";
 export const baseUrl = localUrl;
 
 export const token = {
-	getToken(key) {
-		return uni.getStorageSync(key)
-	},
-	setToken(key, value) {
-		return uni.setStorageSync(key, value);
-	},
-	removeToken(key) {
-		return uni.removeStorageSync(key);
-	}
-}
+  getToken(key) {
+    return uni.getStorageSync(key);
+  },
+  setToken(key, value) {
+    return uni.setStorageSync(key, value);
+  },
+  removeToken(key) {
+    return uni.removeStorageSync(key);
+  },
+};
+
 export const loginStatus = {
-	isLogin() {
-		if (!token.getToken("token")) {
-			uni.reLaunch({
-				url: "/pages/userLogin/userLogin"
-			})
-			token.removeToken('roles');
-			return false;
-		}
-		return true;
-	}
-}
+  isLogin() {
+    if (!token.getToken("token")) {
+      uni.reLaunch({
+        url: "/pages/userLogin/userLogin",
+      });
+      token.removeToken("roles");
+      return false;
+    }
+    return true;
+  },
+  /**
+   * @author 李贺辰
+   * @version 1.0.0
+   * @description 判断登录是否过期
+   * @param {number} code 接口返回的状态码
+   * @param {string} message 接口返回的信息
+   */
+  goLogin(code, message) {
+    if (code == 808) {
+      uni.showToast({
+        title: message,
+        icon: "error",
+        success: () => {
+          setTimeout(() => {
+            uni.reLaunch({
+              url: "/pages/userLogin/userLogin",
+            });
+          }, 1000);
+        },
+      });
+    }
+  },
+};
